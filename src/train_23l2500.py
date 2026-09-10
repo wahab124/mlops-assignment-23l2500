8f3426ccdd3185c62b4d1940db07c357c0f0e3a9
Abdul Wahab <abdul.ahad148@gmail.com>
2026-09-10 10:09:03 -0700

    Add standardization preprocessing 23l2500

--- src/train_23l2500.py
+++ src/train_23l2500.py
@@ -1,10 +1,11 @@
-print("House Price Prediction Model - 2312500")
+print("House Price Prediction Model - 23l2500")
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import joblib
+from sklearn.preprocessing import StandardScaler
 
 
 # Load dataset
@@ -44,13 +45,18 @@ X_train, X_test, y_train, y_test = train_test_split(
     test_size=0.2,
     random_state=42
 )
+# Standardization
+scaler = StandardScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
 print("Training data:", X_train.shape)
 print("Testing data:", X_test.shape)
 
 # Create the model
 model = RandomForestRegressor(
-    n_estimators=200,
+    n_estimators=250,
     random_state=42,
     max_features="sqrt"
 )
