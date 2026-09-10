9b246e7a567cc65caaac20af070c7d4716263022
Abdul Wahab <abdul.ahad148@gmail.com>
2026-09-10 10:14:07 -0700

    Add MinMax scaling 23l2500

--- src/train_23l2500.py
+++ src/train_23l2500.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import joblib
+from sklearn.preprocessing import MinMaxScaler
 
 
 # Load dataset
@@ -44,6 +45,11 @@ X_train, X_test, y_train, y_test = train_test_split(
     test_size=0.2,
     random_state=42
 )
+# Min-Max normalization
+scaler = MinMaxScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
 print("Training data:", X_train.shape)
 print("Testing data:", X_test.shape)
